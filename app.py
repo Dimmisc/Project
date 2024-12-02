af69f45d4e4af5307b504e76015fe89509495f68
Dimmisc <dimamyscen@gmail.com>
2024-12-02 15:29:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
-from flask import Flask
-from flask import render_template
-import matplotlib.pyplot as p
-import matplotlib.patches as Polygon
-
-
-def graph():
-    x = [1, 2, 3, 4, 5]
-    y = [25, 32, 34, 20, 25]
-    poly = Polygon(list(zip(x, y)), facecolor='none', edgecolor='blue')
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/<name>')
-def hello(name):
+@app.route('/mainpage')
+def mainpage():
     return render_template('hello.html', name='hgjkhgyikhujk')
+@app.route('/registration')
+def Regpan():
+    return render_template('Regpan.html', name='hgjkhgyikhujk')
+@app.route('/Grst')
+def Regpan():
+    return render_template('graphics2.html', name='hgjkhgyikhujk')
+@app.route('/Grstcls')
+def Regpan():
+    return render_template('graphics3.html', name='hgjkhgyikhujk')
+@app.route('/Grstwkdys')
+def Regpan():
+    return render_template('graphics1.html', name='hgjkhgyikhujk')
 
 
 if __name__ == '__main__':
