ad51639ca2dd8ac7098f11a29837901287cdc13f
Dimmisc <dimamyscen@gmail.com>
2024-12-05 13:24:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,36 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+import openpyxl
+import sqlite3 as sq
 
-@app.route('/mainpage')
-def mainpage():
-    return render_template('hello.html')
-@app.route('/registration')
-def Regpan():
-    return render_template('Regpan.html')
-@app.route('/Grst')
-def studgr():
-    return render_template('graphics1.html')
-@app.route('/Grstcls')
-def studcl():
-    return render_template('graphics2.html')
-@app.route('/Grstwkdys')
-def studgrds():
-    return render_template('graphics3.html')
+wookbook = openpyxl.load_workbook("sales.xlsx")
+worksheet, dataup = wookbook.active, []
+for i in range(0, worksheet.max_row):
+    c = []
+    for col in worksheet.iter_cols(1, worksheet.max_column):
+        c.append(col[i].value)
+    dataup.append(c)
+for i in range(0, worksheet.max_row):
+    for col in worksheet.iter_cols(1, worksheet.max_column):
+        print(col[i].value, end="\t")
+    print('')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+connection = sq.connect("Students.db")
+cursor = connection.cursor()
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Studentsvisit (
+data TEXT,
+groups TEXT,
+surname TEXT,
+name TEXT,
+patronymic TEXT,
+firstentrance TEXT,
+lastexit TEXT,
+attend TEXT,
+status TEXT
+)
+''')
+c = [tuple(i) for i in dataup[1::]]
+for i in c:
+    cursor.execute('''INSERT INTO Studentsvisit (data, groups, surname, name, patronymic, firstentrance, lastexit, attend, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)''', i)
+
+connection.commit()
+connection.close()
